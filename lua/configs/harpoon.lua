e0cf24010f35e358e7641d0b6d2aeb3231121a00
735l4 <pratibimbakhadka@gmail.com>
2024-04-05 19:38:07 +0545

    chore: update harpoon keybinding

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -5,7 +5,7 @@ print("Hello from harpoon");
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-s>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-m>", function() ui.nav_file(5) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(5) end)
